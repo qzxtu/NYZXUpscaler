02c6851e760282afe13bb4bbb5e21f247100ef3a
Nova <69091361+qzxtu@users.noreply.github.com>
2022-08-27 20:23:47 -0500

    Add files via upload

---
+++ NYZXUpscaler/UI.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace NYZXUpscaler
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UI));
+            this.selectedfile = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sButton2 = new Sipaa.Framework.SButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BxtterLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectedfile
+            // 
+            this.selectedfile.AutoSize = true;
+            this.selectedfile.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectedfile.ForeColor = System.Drawing.Color.White;
+            this.selectedfile.Location = new System.Drawing.Point(114, 199);
+            this.selectedfile.Name = "selectedfile";
+            this.selectedfile.Size = new System.Drawing.Size(0, 14);
+            this.selectedfile.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 14);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Selected File:";
+            // 
+            // sButton2
+            // 
+            this.sButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.sButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.sButton2.BorderRadius = 6;
+            this.sButton2.BorderSize = 0;
+            this.sButton2.FlatAppearance.BorderSize = 0;
+            this.sButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sButton2.ForeColor = System.Drawing.Color.White;
+            this.sButton2.Location = new System.Drawing.Point(12, 305);
+            this.sButton2.Name = "sButton2";
+            this.sButton2.Size = new System.Drawing.Size(319, 40);
+            this.sButton2.TabIndex = 7;
+            this.sButton2.Text = "Upscale";
+            this.sButton2.UseVisualStyleBackColor = false;
+            this.sButton2.Click += new System.EventHandler(this.sButton2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Supported images |*.jpg;*.jpeg;*.png;";
+            this.openFileDialog1.Title = "Select an image";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.BxtterLabel);
+            this.panel1.Location = new System.Drawing.Point(-3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 37);
+            this.panel1.TabIndex = 12;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(308, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "-";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(324, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "x";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // BxtterLabel
+            // 
+            this.BxtterLabel.AutoSize = true;
+            this.BxtterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BxtterLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(158)))), ((int)(((byte)(158)))));
+            this.BxtterLabel.Location = new System.Drawing.Point(12, 12);
+            this.BxtterLabel.Name = "BxtterLabel";
+            this.BxtterLabel.Size = new System.Drawing.Size(95, 16);
+            this.BxtterLabel.TabIndex = 13;
+            this.BxtterLabel.Text = "Nyzx-Upscaler";
+            this.BxtterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(343, 358);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.selectedfile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sButton2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "UI";
+            this.Text = "NYZXUpscaler";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label selectedfile;
+        private System.Windows.Forms.Label label1;
+        private Sipaa.Framework.SButton sButton2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label BxtterLabel;
+    }
+}
---
+++ NYZXUpscaler/UI.cs
@@ -0,0 +1,97 @@
+﻿using DeepAI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NYZXUpscaler
+{
+    public partial class UI : Form
+    {
+        [DllImport("Gdi32.dll")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        private bool Bool;
+
+        private Point point;
+
+        public static string url = "";
+
+        public UI()
+        {
+            InitializeComponent();
+            base.Region = Region.FromHrgn(UI.CreateRoundRectRgn(0, 0, base.Width, base.Height, 20, 20));
+        }
+
+        private void sButton2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(label1.Text))
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK) { label1.Text = openFileDialog1.FileName; } else { }
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to improve this image?", "Nyzx", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    string imagepath = label1.Text.ToString();
+                    DeepAI_API api = new DeepAI_API(apiKey: "f324a1e7-c056-4613-9e27-9cfb0ccad096");
+                    StandardApiResponse resp = api.callStandardApi("waifu2x", new
+                    {
+                        image = File.OpenRead(imagepath),
+                    });
+                    JObject jobject = (JObject)JsonConvert.DeserializeObject((api.objectAsJsonString(resp)));
+                    JToken jtoken = jobject["output_url"];
+                    bool flag = string.IsNullOrEmpty((string)jtoken);
+                    if (flag)
+                    {
+                        jtoken = 0;
+                    }
+                    url = string.Format("{0:n0}", jtoken);
+                    Process.Start(url);
+                    Clipboard.SetText(url);
+                    pictureBox1.LoadAsync(url);
+                    label1.Text = "";
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    label1.Text = "";
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.Bool)
+            {
+                base.Location = new Point(base.Location.X - this.point.X + e.X, base.Location.Y - this.point.Y + e.Y);
+                base.Update();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Bool = false;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Bool = true;
+            this.point = e.Location;
+        }
+    }
+}
